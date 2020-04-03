b3bc1f193323383de455c82f6832773177757a31
LafLaurine <laurine.lafontaine@outlook.fr>
2020-04-03 21:59:00 +0200

    test on proba

--- scripts/Deck.js
+++ scripts/Deck.js
@@ -1,33 +1,55 @@
 var Deck = new function () {
-    this.ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
-    this.suits = ['hearts', 'spades', 'diamonds', 'clubs'];
-    this.deck;
+	this.ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+	this.suits = ['hearts', 'spades', 'diamonds', 'clubs'];
+	this.deck;
 
-    /*
-        Fills up the deck array with cards
-    */
+	this.inequalRanks = ['A', 'A', 'A', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 
-    this.init = function () {
-        this.deck = []; //empty the array
-        for (var suits = 3; suits >= 0; suits--) {
-            for (var ranks = 12; ranks >= 0; ranks--) {
-                this.deck.push(new Card(this.ranks[ranks], this.suits[suits]));
-            }
-        }
-    }
+	/*
+	    Fills up the deck array with cards
+	*/
 
-    /*
-        Shuffles the cards in the deck randomly
-    */
+	this.init = function () {
+		this.deck = []; //empty the array
+		for (var suits = 3; suits >= 0; suits--) {
+			for (var ranks = 12; ranks >= 0; ranks--) {
+				this.deck.push(new Card(this.ranks[ranks], this.suits[suits]));
+			}
+		}
+	}
 
-    this.shuffle = function () {
-        var j, x, i;
-        for (i = this.deck.length; i; i--) {
-            j = Math.floor(Math.random() * i);
-            x = this.deck[i - 1];
-            this.deck[i - 1] = this.deck[j];
-            this.deck[j] = x;
-        }
-    }
+
+	this.initInequally = function () {
+		this.deck = []; //empty the array
+		for (var suits = 3; suits >= 0; suits--) {
+			for (var ranks = 12; ranks >= 0; ranks--) {
+				this.deck.push(new Card(this.inequalRanks[ranks], this.suits[suits]));
+			}
+		}
+	}
+
+	/*
+	    Shuffles the cards in the deck randomly
+	*/
+	this.shuffle = function () {
+		/*var j, x, i;
+		for (i = this.deck.length; i; i--) {
+			j = Math.floor(Math.random() * i);
+			x = this.deck[i - 1];
+			this.deck[i - 1] = this.deck[j];
+			this.deck[j] = x;
+		}*/
+
+		// for 1000 turns
+		// switch the values of two random cards
+		for (var i = 0; i < 1000; i++) {
+			var location1 = Math.floor((Math.random() * this.deck.length));
+			var location2 = Math.floor((Math.random() * this.deck.length));
+			var tmp = this.deck[location1];
+
+			this.deck[location1] = this.deck[location2];
+			this.deck[location2] = tmp;
+		}
+	}
 
 }
