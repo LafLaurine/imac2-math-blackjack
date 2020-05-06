b5c2548860045b9a2fd2e584529b2e258be7a55e
LafLaurine <laurine.lafontaine@outlook.fr>
2020-05-06 21:46:07 +0200

    :sparkles: Kinda better randomised Deck

--- test/Deck.js
+++ test/Deck.js
@@ -1,50 +1,123 @@
-function Card(rank, suit, weight) {
-	this.rank = rank;
-	this.suit = suit;
-	this.weight = weight;
-}
-
-
-let Deck = new function () {
-	this.ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
-	this.suits = ['hearts', 'spades', 'diamonds', 'clubs'];
-	this.deck;
-
-	this.weightInequal = [19, 6.72, 6.72, 6.72, 6.72, 6.72, 6.72, 6.72, 6.72, 6.72, 6.72, 6.72, 6.72] //weight of each element 
-	const totalweightIneq = eval(this.weightInequal.join("+")) //get total weight
-
-	const weighedCards = new Array() //new array to hold "weighted" cards
-	let currentCard = 0
-
-	this.getRandomCardInequal = function () {
-		while (currentCard < this.ranks.length) { //step through each this.ranks element
-			for (i = 0; i < this.weightInequal[currentCard]; i++)
-				weighedCards[weighedCards.length] = this.ranks[currentCard]
-			currentCard++
-		}
-		const randomnumber = Math.floor(Math.random() * totalweightIneq)
-		return weighedCards[randomnumber]
+class Card {
+	constructor(rank,suit) {
+		this.rank = rank;
+		this.suit = suit;
 	}
 
+	key() {
+		return `${this.rank}${this.suit}`;
+	}
+}
+
+//init : 2 % luck to be draw
+
+const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+const suits = ['♡', '♤', '♢', '♧'];
+
+//weight of each element
+const weightInequal = {
+	"A♡" : 4.75,
+	"A♤": 4.75,
+	"A♢": 4.75,
+	"A♧": 4.75
+} 
+const weightIneq = eval(Object.values(weightInequal).join("+"))
+const totalweightIneq = ((100 - weightIneq) / 12) / 4;
+
+const weights = [];
+
+for(let i = 0; i < 4; i++) {
+	weights.push(Object.values(weightInequal)[i]);
+} 
+for(let i = 0; i < 48; i++) {
+	weights.push(totalweightIneq);
+}
+
+for(let i = 0; i < 12; i++) {
+	weightInequal[ranks[i + 1] + "♡"] = totalweightIneq;
+	weightInequal[ranks[i + 1] + "♤"] = totalweightIneq;
+	weightInequal[ranks[i + 1] + "♢"] = totalweightIneq;
+	weightInequal[ranks[i + 1] + "♧"] = totalweightIneq;
+}
+
+class Deck {
+	constructor() {
+		this.init();
+	} 
 	/*
 	    Fills up the deck array with cards
 	*/
 
-	this.init = function () {
+	init() {
 		this.deck = []; //empty the array
-		for (let suits = 3; suits >= 0; suits--) {
-			for (let ranks = 12; ranks >= 0; ranks--) {
-				this.deck.push(new Card(this.ranks[ranks], this.suits[suits], this.weightInequal[ranks]));
+		for (let s = 3; s >= 0; s--) {
+			for (let r = 12; r >= 0; r--) {
+				this.deck.push(new Card(ranks[r], suits[s]));
 			}
 		}
 	}
 
+	draw() {
+		return this.deck.splice(0,1)[0];
+	}
+
+	shuffle() {
+		for (let i = this.deck.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[this.deck[i], this.deck[j]] = [this.deck[j], this.deck[i]];
+		}
+	}
+	
+	shuffleInequal() {
+		const sampler = new WeightedSampler(Object.keys(weightInequal), weights);
+		let randomArray = Array.apply(null, Array(this.deck.length)).map(() => sampler.get());
+		const index = sampler.elements.indexOf(sampler.get());
+		sampler.elements.splice(index, 1);
+		this.deck = randomArray;
+	}
+
+	drawInequal() {
+		return this.deck.splice(0,1)[0];
+	}
 }
 
-Deck.init()
-console.log(Deck.deck)
-const container = document.getElementById('truc');
-for (let i = 0; i < 52; i++) {
-	container.append(" " + Deck.getRandomCardInequal());
-	console.log(Deck.getRandomCardInequal())
+class WeightedSampler {
+    constructor(elements, weights) {
+        this.total = 0;
+        this.elements = Array.from(elements);
+		this.cweights = weights.map(weight => this.total += weight);
+    }
+    get() {
+		let random = Math.random() * this.total;
+        return this.elements.find((element, index) => random < this.cweights[index]);
+    }
+}
+
+let deck = new Deck();
+
+/*
+for(let tirage = 1;tirage<=10;tirage++)
+{
+	deck.init();
+	deck.shuffle();
+	document.getElementById("truc").append(`tirage ${tirage}`);
+	document.getElementById("truc").append(document.createElement("br"));
+	for(let i =0 ;i<10;i++)
+		document.getElementById("truc").append(deck.draw().key() + "     ");
+
+	document.getElementById("truc").append(document.createElement("br"))
+}*/
+
+deck.init();
+
+for(let tirage = 1;tirage<=10;tirage++)
+{
+	deck.init();
+	deck.shuffleInequal();
+	document.getElementById("truc").append(`tirage ${tirage}`);
+	document.getElementById("truc").append(document.createElement("br"));
+	for(let i =0 ;i<10;i++) {
+		document.getElementById("truc").append(deck.drawInequal() + "     ");
+	}
+	document.getElementById("truc").append(document.createElement("br"))
 }
